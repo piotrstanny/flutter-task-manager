f6bc8e0236c1216c12b0c4f5671958039031f7b8
Piotr Stanny <piotr.stanny@gmail.com>
2020-09-04 11:15:54 +0100

    Add a screen struture

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Color(0xffCDDC39),
+        body: Column(
+          children: <Widget>[
+            Icon(Icons.format_list_bulleted),
+            Text('Task Manager'),
+          ],
+        ));
   }
 }
